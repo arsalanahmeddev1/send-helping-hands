f207f5456765fb451e3c192745e6028a81bd1490
Muhammad Saqib <saqibimran409@gmail.com>
2024-04-25 16:15:13 +0500

    made some pages

---
+++ blogs.php
@@ -0,0 +1,52 @@
+<?php include "./include/head.php"; ?>
+<?php include "./include/header.php"; ?>
+<section class="blog-banner banner-props">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="banner-content pb-75">
+                    <h1 class="banner-heading  mb-30">Blogs</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="blog-sec-1 pt-100 pb-100">
+    <div class="container-fluid">
+        <h6 class="sec-title text-uppercase text-center">Blog</h6>
+        <h2 class="sec-heading text-center ">Helping you build resilience and lead fulfilling lives</h2>
+        <div class="row mt-80">
+         <div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="image-box1 radius-20 mb-40">
+                <p class="fw-700 fs-28 text-white mb-0 img-box-desc"> <span class="fs-17 fw-700">Date: 3-7-2023</span> <br> SEND Helping Hands establish trauma training institute in UK</p>
+             </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="image-box2 radius-20 mb-40">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc"><span class="fs-17 fw-700">Date: 3-7-2023</span> <br> Supporting a Healthy Mind Through Diet & Exercise</p>
+             </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="image-box3 radius-20 mb-40">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc"><span class="fs-17 fw-700">Date: 3-7-2023</span> <br> Supporting the Mental Health of your family</p>
+             </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="image-box4 radius-20">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc"><span class="fs-17 fw-700">Date: 3-7-2023</span> <br> Promotion Campaign For <br> Telemedicine Services</p>
+             </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="image-box5 radius-20">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc"><span class="fs-17 fw-700">Date: 3-7-2023</span> <br> Mental Health <br> Campaign</p>
+             </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="image-box6 radius-20">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc"><span class="fs-17 fw-700">Date: 3-7-2023</span> <br> Nursing Scholaship <br> Program</p>
+             </div>
+         </div>
+     </div>
+    </div>
+</section>
+<?php include "./include/footer.php"; ?>
---
+++ who-we-help.php
@@ -0,0 +1,38 @@
+<?php include "./include/head.php"; ?>
+<?php include "./include/header.php"; ?>
+<section class="we-help-banner banner-props">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="banner-content pb-75">
+                    <h1 class="banner-heading  mb-30">What we do</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="blog-sec-1 pt-100 pb-100">
+    <div class="container-fluid">
+        <h6 class="sec-title text-uppercase text-center">What we do</h6>
+        <h2 class="sec-heading text-center ">Helping you build resilience <br> and lead fulfilling lives</h2>
+        <?php include "./include/we-help.php"; ?>
+        <div class="row mt-40">
+         <div class="col-lg-4">
+             <div class="image-box1 radius-20 mb-40">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc">Addiction</p>
+             </div>
+         </div>
+         <div class="col-lg-4">
+             <div class="image-box2 radius-20 mb-40">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc">Eating disorders</p>
+             </div>
+         </div>
+         <div class="col-lg-4">
+             <div class="image-box3 radius-20 mb-40">
+                 <p class="fw-700 fs-28 text-white mb-0 img-box-desc">Weight management</p>
+             </div>
+         </div>
+     </div>
+    </div>
+</section>
+<?php include "./include/footer.php"; ?>
